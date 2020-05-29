63fb97f2ecdbe0b63e5dc2c75d3a78adbdc365db
phanendraguptha <www.phani122@gmail.com>
2020-05-29 12:31:15 +0530

    deploying

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+require('mongoose-type-email');
+
+var contactSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dob: String,
+  phone1: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone2: {
+    type: Number,
+    allowBlank: true
+  },
+  email1: {
+    type: mongoose.SchemaTypes.Email,
+    required: true
+  },
+  email2: {
+    type: mongoose.SchemaTypes.Email,
+    allowBlank: true
+  }
+});
+
+var Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
